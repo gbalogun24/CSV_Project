637465bda2df6fe37a4a4b4e148666fb1a758e50
gafar <gb262@njit.edu>
2018-10-07 18:13:00 -0400

    Feature: Created the CSVCommands class and function to read csv file (In progress)

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
 
-//CSV file class
-class CSV{
+$csv = "/home/gafar/PhpstormProjects/CSV_new/file.csv";
+
+main::Program($csv);
 
 
-    public function _contruct($file){
-        $this->csvfile = $file;
+class  main {
+
+    public static function Program($csv){
+        $csvrecords = CSVCommands::readCSV($csv);
+        print_r($csvrecords);
     }
-//Function to return csv file
-    public function getCSV(){
-        return $this->csvfile;
-    }
-
 }
+
+
 //Class for csv record object
 class csvrecord{
 
@@ -26,6 +27,41 @@ class csvrecord{
 
 }
 
+//Record factory
+class csvrecordsFactory{
 
+//Function to create array of records
+    public static function createCSVRecords(Array $fieldnames, $values){
+        $csvrecords = new csvrecord($fieldnames, $values);
+        print_r($csvrecords);
+        return $csvrecords;
+    }
+
+}
+
+//Class for CSV commands
+class CSVCommands {
+
+    //Read the csv file
+    public static function readCSV($file){
+
+        $readfile = fopen($file,"r");
+        $fieldnames = array();
+        $count = 0;
+        //Loop through the file
+        while (($csvtext = fgetcsv($readfile, 50000,",")) !== FALSE) {
+            if($count == 0){
+                $fieldnames = $readfile;
+            }
+            else{
+                $csvrecords[] = csvrecordsFactory::createCSVRecords($fieldnames, $csvtext);
+            }
+            // $csvrecords[] = $csvtext;
+            $count++;
+        }
+        fclose($readfile);
+        return $csvrecords;
+    }
+}
 
 ?>
